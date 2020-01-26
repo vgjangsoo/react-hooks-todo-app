35fc39640e0b5dfeb1f2fb1689202c5b0b6bdeff
Sungil Saul Lee <jangsoo86@gmail.com>
2020-01-26 07:23:36 -0800

    new todos can be shown in the console

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import './App.css';
 
 function App() {
   const [newTodo, setNewTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  function handleNewTodoChange(e) {
+    e.preventDefault();
+    setNewTodo(e.target.value);
+  };
+  function handleNewTodo(e) {
+    e.preventDefault();
+    console.log(newTodo);
+  }
 
   return (
     <div className="demo-component">
       <h1>Todos</h1>
-      <form>
-        <input placeholder="Your todo..."/>
+      <form onSubmit={handleNewTodo}>
+        <input placeholder="Your todo..." onChange={handleNewTodoChange} />
         <ul>
           <li>Buy Milk</li>
           <li>Buy Cheese</li>
@@ -16,6 +26,6 @@ function App() {
       </form>
     </div>
   );
-}
+};
 
 export default App;
