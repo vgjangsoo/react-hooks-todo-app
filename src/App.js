a5dbed8f8d88d5cf455dc7fbde90a98373808427
Sungil Saul Lee <jangsoo86@gmail.com>
2020-01-26 07:10:12 -0800

    basic HTML

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState }from 'react';
 import './App.css';
 
 function App() {
+  const [newTodo, setNewTodo] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="demo-component">
+      <h1>Todos</h1>
+      <form>
+        <input placeholder="Your todo..."/>
+        <ul>
+          <li>Buy Milk</li>
+          <li>Buy Cheese</li>
+        </ul>
+      </form>
     </div>
   );
 }
